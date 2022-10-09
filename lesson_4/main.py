94f2053ff21a20c1f26d2bb721ccb531a2c76ead
movavi_school <klimnesterov8@gmail.com>
2022-10-09 13:12:30 +0700

    09.10.2022

---
+++ lesson_4/main.py
@@ -0,0 +1,32 @@
+# x = 'строчка'
+# x1 = "строчка"
+# name = "антон\nантон\nантон"
+# name1 =''''''
+# строка 1
+# строка 2
+# строка 3
+# print(name)
+# st = "я большой"
+# print(st.upper())
+# st1 = "Я МАЛЕНЬКИЙ"
+# print(st1.lower())
+# st2 = "Я фраза и хочу начинаться с большой"
+# print(st2.capitalize())
+# familiya = input("фамилия: ")
+# imya = input("имя: ")
+# otchestvo = input("отчество: ")
+# print(familiya + imya[0] + "." + otchestvo[0] + ".")
+# st = input("Введи слово/фразу: ")
+# print(st.count("a"))
+# print(st.lower("a"))
+# phrase = input("Введите слова через зарятую: ")
+# lst = (phrase.split(","))
+# lst =lst.pop(0)
+# print(lst)
+# phrase = input("Введи строчку:")
+# find = input("Что меняем:")
+# replace = input("На что меняем:")
+# print(phrase.replace(find, replace))
+# phrase = input("Введи строчку:")
+# print(phrase.replace("
+
