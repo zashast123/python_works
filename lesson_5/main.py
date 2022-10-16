4f76ed5e560515e551da2766ed50a582340d7a8e
movavi_school <uvukoska@gmail.com>
2022-10-16 13:14:28 +0700

    16.10.2022

---
+++ lesson_5/main.py
@@ -0,0 +1,66 @@
+# x = 7
+# if x <= 10 or x == 15:
+#     print("икс не больше 10 или равен 15")
+# else:
+#     print("икс больше 10")
+# x = int(input("введите число:"))
+# if x < 0:
+#     print("отрицательное")
+# elif x > 0:
+#     print("Положительное")
+# else:
+#     print("нолик")
+# year = int(input("Введите год:"))
+# if year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+#     print("Високосный")
+# else:
+#     print("Не високосный")
+# number_1 = int(input("Введите первое число"))
+#  operation = input("Введи операцию(+,-,*,/)")
+#  number_2 = int(input("Введи второе число"))
+#  lst =["+","-","/","*"]
+# if operation in lst:
+#      print("Я сработ
+# number_1 = int(input("Введите первое число"))
+# number_2 = int(input("Введи второе число"))
+# number_3 = int(input("Введи третье число"))
+# counter_plus = 0
+# counter_minus = 0
+# if number_2 > 0:
+#     counter_plus += 1
+# else:
+#     counter_minus += 1
+#
+# if number_1 > 0:
+#     counter_plus += 1
+# else:
+#     counter_minus += 1
+#
+# if number_3 > 0:
+#     counter_plus += 1
+# else:
+#     counter_minus += 1
+# print("Положительных:",counter_plus)
+# print("Отрицательных:",counter_minus)
+# number_1 = int(input("Введите первое число"))
+# number_2 = int(input("Введи второе число"))
+# number_3 = int(input("Введи третье число"))
+# lst = [number_1,number_2,number_3]
+# mini = min(lst)
+# maxi = max(lst)
+# print("Минимальное", mini)
+# print("Максимальное",maxi)
+# ticket = input("Введите номер билета:")
+# if len(ticket) == 6:
+#     first_half = ticket[:3]
+#     second_half =ticket[-3:]
+#
+#     first_sum = int(first_half[0]) + int(first_half[1]) + int(first_half[2])
+#     second_sum =int(second_half[0]) + int(second_half[1]) + int(second_half[2])
+#
+#     if first_sum == second_sum:
+#         print("Твой билет счастливый!")
+#     else:
+#         print("Не повезло(")
+# else:
+#     print("Не то ты ввёл")
